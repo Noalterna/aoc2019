f9fdfdedad2c035a9aa9c538926deeeb2eef316e
Noalterna <noalterna@gmail.com>
2019-12-09 21:39:26 +0100

    day 1 part 2

--- day1.kt
+++ day1.kt
@@ -2,25 +2,33 @@ import java.io.File
 import kotlin.math.*
 
 fun readFile(): MutableList<String>{
-    val fileName = "d:/advent2019/day1.txt"
+    val fileName = "./day1.txt"
     val myList = mutableListOf<String>()
     File(fileName).useLines { lines -> myList.addAll(lines)}
    return myList
 }
 
-fun fuel(mass:Int): Double{
-    val x = mass.toDouble()/3
+fun fuel(mass:Double): Double{
+    val x = mass/3
     return floor(x) - 2
 }
 
 fun main(args: Array<String>){
-    val massListEx = mutableListOf(12, 14, 1969, 100756)
-    massListx.forEach{println(fuel(it))}
+    //val massListEx = mutableListOf(12.0, 14.0, 1969.0, 100756.0)
+    //massListx.forEach{println(fuel(it))}
 
     val massList = readFile()
-    val fuelList = mutableListOf<Double>()
-    massList.forEach{ fuelList.add(fuel(it.toInt()))}
-    fuelList.forEach{println(it)}
+    val fuelList = mutableListOf<Double>(14.0)
+    massList.forEach{ fuelList.add(fuel(it.toDouble()))}
+   println("Sum of fuel " + fuelList.sum())
 
-    println("Sum of all fuel " + fuelList.sum())
+    var sum = 0.0
+    massList.forEach {
+        var current = it.toDouble()
+        while(fuel(current) > 0) {
+            sum = sum + fuel(current)
+            current = fuel(current)
+        }
+    }
+    println("Sum with additional fuel " + sum)
 }
