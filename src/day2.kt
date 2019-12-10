2985ffc163fa63cfcfd3fcbeef30415719f2c89c
Noalterna <noalterna@gmail.com>
2019-12-10 17:37:30 +0100

    day 2 part 2

--- src/day2.kt
+++ src/day2.kt
@@ -24,17 +24,36 @@ fun computer(intCodeProgram :MutableList<Int>){
     }
 }
 
+fun part2(fileName: String): List<Int>{
+    var initial_program = readFileToList(fileName)
+    for(noun in 0..99){
+        for(verb in 0..99){
+            initial_program = readFileToList(fileName)
+            initial_program[1] = noun
+            initial_program[2] = verb
+            computer(initial_program)
+            if(initial_program[0] == 19690720){
+                return listOf(initial_program[1],initial_program[2])
+            }
+        }
+    }
+    return listOf(100,100)
+}
+
 fun main(){
     val fileName = "resources/day2.txt"
-    val intCodeProgram: MutableList<Int> = readFileToList(fileName)
+    var intCodeProgram: MutableList<Int> = readFileToList(fileName)
 
     /*val example = mutableListOf(1,1,1,4,99,5,6,0,99)
     computer(example)
     example.forEach{ println(it)}*/
-    
+
     intCodeProgram[1] = 12
     intCodeProgram[2] = 2
     computer(intCodeProgram)
-    intCodeProgram.forEach{println(it)}
-    println(intCodeProgram[0])
+    println("Part 1: " + intCodeProgram[0])
+
+    val result_part2= part2(fileName)
+    result_part2.forEach{println(it)}
+    println("Part 2 " + 100*result_part2[0] + result_part2[1])
 }
