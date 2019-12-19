42b0edfcacfca7bbaeb73a1716b1499e2c736f1b
Noalterna <noalterna@gmail.com>
2019-12-19 21:19:01 +0100

    Day 6 part 2

--- src/day6.kt
+++ src/day6.kt
@@ -1,9 +1,9 @@
 import java.io.File
-fun getPath( orbitMap :Map<String, List<String>>, current :String, path :List<String> = emptyList() ):List<String> {
+fun getPaths( orbitMap :Map<String, List<String>>, current :String, path :List<String> = emptyList() ):List<String> {
 	return if(!orbitMap.containsKey(current)) path
 	else {
 		val next = orbitMap.getValue(current).first().toString()
-		getPath(orbitMap, next, path + current)
+		getPaths(orbitMap, next, path + current)
 	}
 }
 fun main() {
@@ -12,15 +12,15 @@ fun main() {
 		.map{ it.split(')') }
 		.groupBy( { it.last() }, { it.first() } )
 	val pathSize = orbitMap.keys
-		.map{ getPath(orbitMap, it) }
+		.map{ getPaths(orbitMap, it) }
 		.flatten().size
 	println("Part 1: " + pathSize)
 
-	//Example
-	val example = listOf<String>("COM)B","B)C","C)D","D)E","E)F","B)G", "G)H","D)I","E)J","J)K","K)L")
-	val inne = example.map{ it.split(')') }
-		.groupBy( { it.last() }, { it.first() } )
-	val x = inne.keys.map{ getPath(inne, it)}.flatten().size
+	val pathFromYou = getPaths(orbitMap, "YOU").drop(1)
+	val pathFromSan = getPaths(orbitMap,"SAN").drop(1)
+	val pathYouToSan = pathFromYou + pathFromSan - (pathFromSan intersect pathFromYou)
+	println("Part 2: " + pathYouToSan.size)
+
 
 
 
